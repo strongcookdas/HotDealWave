a75cb21f687a74819caa8237ee7fd7f98072c8c0
MCshin00 <sinminc7@gmail.com>
2025-01-01 16:30:28 +0900

    chore: coupon 프로젝트 생성

---
+++ coupon/src/main/java/com/sparta/hotdeal/coupon/CouponApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hotdeal.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+	}
+
+}
---
+++ coupon/src/test/java/com/sparta/hotdeal/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hotdeal.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
