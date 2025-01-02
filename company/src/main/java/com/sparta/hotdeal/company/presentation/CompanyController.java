313e2a1a3b372bbcee9c73a0e8149b0bc8a7a63e
sooooooongyi <sooooooongyi@gmail.com>
2025-01-02 17:41:24 +0900

    feat: 업체 Controller 작성 (#6)

--- company/src/main/java/com/sparta/hotdeal/company/presentation/CompanyController.java
+++ company/src/main/java/com/sparta/hotdeal/company/presentation/CompanyController.java
@@ -1,9 +1,72 @@
 package com.sparta.hotdeal.company.presentation;
 
+import com.sparta.hotdeal.company.domain.entity.CompanyStatusEnum;
+import com.sparta.hotdeal.company.presentation.dtos.ReqPatchCompanyByIdDto;
+import com.sparta.hotdeal.company.presentation.dtos.ReqPatchCompanyByIdStatusDto;
+import com.sparta.hotdeal.company.presentation.dtos.ReqPostCompanyDto;
+import com.sparta.hotdeal.company.presentation.dtos.ResGetCompanyByIdDto;
+import com.sparta.hotdeal.company.presentation.dtos.ResponseDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/companies")
 public class CompanyController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDto<Void> createCompany(@RequestBody ReqPostCompanyDto reqPostCompanyDto) {
+        return ResponseDto.of("업체가 신청되었습니다.", null);
+    }
+
+    @PatchMapping("/{companyId}")
+    public ResponseDto<Void> updateCompany(@PathVariable UUID companyId,
+                                           @RequestBody ReqPatchCompanyByIdDto reqPatchCompanyByIdDto) {
+        return ResponseDto.of("업체가 수정되었습니다.", null);
+    }
+
+    @PatchMapping("/{companyId}/approve")
+    public ResponseDto<Void> updateCompanyStatus(@PathVariable UUID companyId,
+                                                 @RequestBody ReqPatchCompanyByIdStatusDto reqPatchCompanyByIdStatusDto) {
+        return ResponseDto.of("업체가 승인되었습니다.", null);
+    }
+
+    @GetMapping("/{companyId}")
+    public ResponseDto<ResGetCompanyByIdDto> getCompanyById(@PathVariable UUID companyId) {
+        ResGetCompanyByIdDto resGetCompanyByIdDto = ResGetCompanyByIdDto.builder()
+                .companyEmail("test1@test.com")
+                .companyPhoneNumber(12345678L)
+                .brandName("testBrandName")
+                .businessRegistrationNumber(12345678L)
+                .managerId(UUID.randomUUID())
+                .status(CompanyStatusEnum.PENDING)
+                .build();
+        return ResponseDto.of("업체가 조회되었습니다.", resGetCompanyByIdDto);
+    }
+
+    @GetMapping
+    public ResponseDto<List<ResGetCompanyByIdDto>> getCompanyList() {
+        ResGetCompanyByIdDto resGetCompanyByIdDto = ResGetCompanyByIdDto.builder()
+                .companyEmail("test1@test.com")
+                .companyPhoneNumber(12345678L)
+                .brandName("testBrandName")
+                .businessRegistrationNumber(12345678L)
+                .managerId(UUID.randomUUID())
+                .status(CompanyStatusEnum.PENDING)
+                .build();
+
+        List<ResGetCompanyByIdDto> responseDtos = new ArrayList<>();
+        responseDtos.add(resGetCompanyByIdDto);
+        return ResponseDto.of("업체목록이 조회되었습니다.", responseDtos);
+    }
 }
