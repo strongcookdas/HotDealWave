66de5a97019223ba54629402cd50fcdba897bebb
yewonkim301 <yeowne301@gmail.com>
2025-01-02 16:39:40 +0900

    chore: product service 내 상품 관련 ENUM 값 세팅(#5)

--- product/src/main/java/com/sparta/hotdeal/product/domain/entity/product/ProductStatusEnum.java
+++ product/src/main/java/com/sparta/hotdeal/product/domain/entity/product/ProductStatusEnum.java
@@ -1,4 +1,9 @@
 package com.sparta.hotdeal.product.domain.entity.product;
 
 public enum ProductStatusEnum {
+    ON_SALE, // 판매중
+    DISCONTINUED, // 판매 중지
+    OUT_OF_STOCK,// 재고 부족
+    SOLD_OUT, // 매진
+    INACTIVE; // 비활성화 (임시로 판매 중단됨)
 }
