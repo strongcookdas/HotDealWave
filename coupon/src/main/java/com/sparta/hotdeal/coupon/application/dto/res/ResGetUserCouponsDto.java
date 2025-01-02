811e7b7ffa1e11b39fcbbd2dbce06432e7cd2e5b
pk_hyun <pkh954533@gmail.com>
2025-01-02 16:02:11 +0900

    fix: ResGetUserCouponsDto 간소화

--- coupon/src/main/java/com/sparta/hotdeal/coupon/application/dto/res/ResGetUserCouponsDto.java
+++ coupon/src/main/java/com/sparta/hotdeal/coupon/application/dto/res/ResGetUserCouponsDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -14,20 +13,12 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResGetUserCouponsDto {
-    private List<CouponInfo> coupons;
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class CouponInfo {
-        private UUID couponId;
-        private String name;
-        private int discountAmount;
-        private int minOrderAmount;
-        private LocalDate expirationDate;
-        private boolean isUsed;
-        private LocalDate usedDate;
-        private LocalDate dailyIssuedDate;
-    }
+    private UUID couponId;
+    private String name;
+    private int discountAmount;
+    private int minOrderAmount;
+    private LocalDate expirationDate;
+    private boolean isUsed;
+    private LocalDate usedDate;
+    private LocalDate dailyIssuedDate;
 }
