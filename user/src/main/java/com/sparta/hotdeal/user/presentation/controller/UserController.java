b0b2798efffc5e3da7d194372802c9327a2a2fe0
MCshin00 <sinminc7@gmail.com>
2025-01-02 14:47:10 +0900

    feat: 회원정보조회 API 컨트롤러 및 더미데이터 리턴 구현 (#4)

---
+++ user/src/main/java/com/sparta/hotdeal/user/presentation/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.sparta.hotdeal.user.presentation.controller;
+
+import com.sparta.hotdeal.user.presentation.dtos.ResponseDto;
+import com.sparta.hotdeal.user.presentation.dtos.users.response.ResGetUserDto;
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseDto<ResGetUserDto>> getUser(@PathVariable("userId") UUID userId) {
+
+        ResGetUserDto resGetUserDto = ResGetUserDto.builder()
+                .userId(userId)
+                .email("example@email.com")
+                .nickname("exampleNickname")
+                .defaultAddressId(UUID.randomUUID())
+                .build();
+
+        ResponseDto<ResGetUserDto> responseDto = ResponseDto.of(200, "회원정보조회 성공", resGetUserDto);
+
+        return ResponseEntity.ok(responseDto);
+    }
+}
