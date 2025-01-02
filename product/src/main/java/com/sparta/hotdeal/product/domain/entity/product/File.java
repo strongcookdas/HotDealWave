93a603208858ea5dc8850b026005257dbc33795d
yewonkim301 <yeowne301@gmail.com>
2025-01-02 15:32:45 +0900

    chore: product service 내 product 관련 entity 작성(#5)

---
+++ product/src/main/java/com/sparta/hotdeal/product/domain/entity/product/File.java
@@ -0,0 +1,42 @@
+package com.sparta.hotdeal.product.domain.entity.product;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "p_file")
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // SubFile과 1:N 관계 설정
+    @OneToMany(mappedBy = "file", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SubFile> subFiles;
+
+    // Product와 detailImgs의 1:1 관계
+    @OneToOne(mappedBy = "detailImgs")
+    private Product productDetailImgs;
+
+    // Product와 thumbImg의 1:1 관계
+    @OneToOne(mappedBy = "thumbImg")
+    private Product productThumbImg;
+}
---
+++ product/src/main/java/com/sparta/hotdeal/product/domain/entity/product/Promotion.java
@@ -0,0 +1,49 @@
+package com.sparta.hotdeal.product.domain.entity.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "p_promotion")
+public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private LocalDateTime start;
+
+    @Column(nullable = false)
+    private LocalDateTime end;
+
+    @Column(nullable = false)
+    private Integer discountRate;
+
+    private Integer discountPrice;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
