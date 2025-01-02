55abd335f140c2f254b6003ad147aec3f2979592
strongcookdas <goodjav141592@gmail.com>
2025-01-02 16:54:00 +0900

    feat : Order Controller 더미 데이터 반환 (#3)

---
+++ order/src/main/java/com/sparta/hotdeal/order/presentation/dtos/order/ReqPostOrderDto.java
@@ -0,0 +1,29 @@
+package com.sparta.hotdeal.order.presentation.dtos.order;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqPostOrderDto {
+    private List<OrderedProduct> orderedProducts;
+    private String addressId;
+    private String orderStatus;
+    private String couponId;
+
+    @Getter
+    @Builder
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OrderedProduct {
+        private String basketId;
+    }
+}
