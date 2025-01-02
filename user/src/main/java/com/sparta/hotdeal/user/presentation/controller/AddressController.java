6f1d17cc18de0039d622afc6172f78e5b63117b4
MCshin00 <sinminc7@gmail.com>
2025-01-02 16:36:03 +0900

    feat: 배송지 조회 API 컨트롤러 및 더미데이터 리턴 구현 (#4)

--- user/src/main/java/com/sparta/hotdeal/user/presentation/controller/AddressController.java
+++ user/src/main/java/com/sparta/hotdeal/user/presentation/controller/AddressController.java
@@ -2,11 +2,16 @@ package com.sparta.hotdeal.user.presentation.controller;
 
 import com.sparta.hotdeal.user.presentation.dtos.ResponseDto;
 import com.sparta.hotdeal.user.presentation.dtos.address.request.ReqPostAddressDto;
+import com.sparta.hotdeal.user.presentation.dtos.address.response.ResGetAddressByIdDto;
 import com.sparta.hotdeal.user.presentation.dtos.address.response.ResPostAddressDto;
 import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddressController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseDto<ResPostAddressDto> createAddress(@RequestBody ReqPostAddressDto requestDto) {
 
         ResPostAddressDto resPostAddressDto = ResPostAddressDto.builder()
@@ -21,5 +27,21 @@ public class AddressController {
                 .build();
 
         return ResponseDto.of("배송지 등록 성공", resPostAddressDto);
+    }
+
+    @GetMapping("/{addressId}")
+    public ResponseDto<ResGetAddressByIdDto> getAddress(@PathVariable UUID addressId) {
+
+        ResGetAddressByIdDto resGetAddressByIdDto = ResGetAddressByIdDto.builder()
+                .addressId(addressId)
+                .zipNum("zipNum")
+                .city("city")
+                .district("district")
+                .streetName("streetName")
+                .streetNum("streetNum")
+                .detailAddr("detailAddr")
+                .build();
+
+        return ResponseDto.of("배송지 조회 성공", resGetAddressByIdDto);
     }
 }
