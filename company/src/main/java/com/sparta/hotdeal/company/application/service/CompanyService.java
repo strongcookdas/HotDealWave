519e7e63ec6626175a64def9187a896ffc2692d4
sooooooongyi <sooooooongyi@gmail.com>
2025-01-03 16:32:53 +0900

    feat: 업체 생성 구현

--- company/src/main/java/com/sparta/hotdeal/company/application/service/CompanyService.java
+++ company/src/main/java/com/sparta/hotdeal/company/application/service/CompanyService.java
@@ -10,4 +10,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CompanyService {
     private final CompanyRepository companyRepository;
+
+    public void createCompany(ReqPostCompanyDto reqPostCompanyDto) {
+        Company company = Company.create(
+                reqPostCompanyDto.getBusinessRegistrationNumber(),
+                reqPostCompanyDto.getCompanyPhoneNumber(),
+                reqPostCompanyDto.getManagerId(),
+                reqPostCompanyDto.getCompanyEmail(),
+                reqPostCompanyDto.getBrandName(),
+                reqPostCompanyDto.getStatus()
+        );
+        companyRepository.save(company);
+    }
 }
