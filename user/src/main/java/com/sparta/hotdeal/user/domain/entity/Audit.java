d4985edffaf43a0110648d174afe0e4dfe8c88a1
MCshin00 <sinminc7@gmail.com>
2025-01-04 14:35:24 +0900

    feat: Auditing 기능 추가 (#20)

---
+++ user/src/main/java/com/sparta/hotdeal/user/domain/entity/Audit.java
@@ -0,0 +1,43 @@
+package com.sparta.hotdeal.user.domain.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Audit {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    private LocalDateTime deletedAt;
+
+    private String deletedBy;
+
+    public void updateCreatedByAndLastModifiedBy(String username) {
+        createdBy = username;
+        updatedBy = username;
+    }
+
+    public void updateDeleted(String username) {
+        deletedBy = username;
+        deletedAt = LocalDateTime.now();
+    }
+}
