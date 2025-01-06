96f3e08c1b81d906e50201f53c959d291de929b2
yewonkim301 <yeowne301@gmail.com>
2025-01-06 19:07:30 +0900

    refactor: ReqPostPromotionDto 유효성 검증 추가(#31)

--- product/src/main/java/com/sparta/hotdeal/product/application/dtos/req/product/ReqPostPromotionDto.java
+++ product/src/main/java/com/sparta/hotdeal/product/application/dtos/req/product/ReqPostPromotionDto.java
@@ -1,5 +1,8 @@
 package com.sparta.hotdeal.product.application.dtos.req.product;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -10,9 +13,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class ReqPostPromotionDto {
+    @NotNull(message = "상품을 입력해주세요.")
     private UUID productId;
+
+    @NotNull(message = "타임 세일 시작 시간을 입력해주세요.")
     private LocalDateTime start;
+
+    @NotNull(message = "타임 세일 종료 시간을 입력해주세요.")
     private LocalDateTime end;
+
+    @Positive(message = "가격은 1 이상으로 입력해주세요.")
+    @Max(value = 2100000000, message = "가격은 2100000000 이하로 입력해주세요.")
     private Integer discountPrice;
+
+    @Positive(message = "수량은 1 이상으로 입력해주세요.")
+    @Max(value = 2100000000, message = "수량은 2100000000 이하로 입력해주세요.")
     private Integer quantity;
 }
