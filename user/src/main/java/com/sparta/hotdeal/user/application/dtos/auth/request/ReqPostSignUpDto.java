af6dee2dd1cc4b8ecffe5a7daf0c3b70982a642d
MCshin00 <sinminc7@gmail.com>
2025-01-08 00:13:16 +0900

    refactor: 회원가입 시 필드 검증 추가 (#45)

--- user/src/main/java/com/sparta/hotdeal/user/application/dtos/auth/request/ReqPostSignUpDto.java
+++ user/src/main/java/com/sparta/hotdeal/user/application/dtos/auth/request/ReqPostSignUpDto.java
@@ -2,14 +2,24 @@ package com.sparta.hotdeal.user.application.dtos.auth.request;
 
 import com.sparta.hotdeal.user.domain.entity.User;
 import com.sparta.hotdeal.user.domain.entity.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ReqPostSignUpDto {
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+    @NotBlank
     private String email;
+    @NotBlank
+    @Size(min = 8, max = 15)
+    @Pattern(regexp = "^[a-zA-Z\\d!@#$%^&*()_+\\-=]*$")
     private String password;
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]+$")
     private String nickname;
     private UserRole role;
 
