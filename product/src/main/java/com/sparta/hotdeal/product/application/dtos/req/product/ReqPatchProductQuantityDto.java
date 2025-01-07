8d7ce12a07400d3d0eb8670c20103a5b699ad58e
yewonkim301 <yeowne301@gmail.com>
2025-01-07 14:49:41 +0900

    refactor: 상품 재고 수량 차감, 복구하는 dto 생성(#36)

---
+++ product/src/main/java/com/sparta/hotdeal/product/application/dtos/req/product/ReqPatchProductQuantityDto.java
@@ -0,0 +1,14 @@
+package com.sparta.hotdeal.product.application.dtos.req.product;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ReqPatchProductQuantityDto {
+    private UUID productId;
+    private int quantity;
+}
