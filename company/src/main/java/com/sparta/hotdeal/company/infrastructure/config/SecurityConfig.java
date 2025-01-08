b1ba7f0ac36d860b3f0cebbd63a1a8bfd967ca15
sooooooongyi <sooooooongyi@gmail.com>
2025-01-08 18:22:52 +0900

    refactor: SecurityConfig 수정 및 핸들러 추가

--- company/src/main/java/com/sparta/hotdeal/company/infrastructure/config/SecurityConfig.java
+++ company/src/main/java/com/sparta/hotdeal/company/infrastructure/config/SecurityConfig.java
@@ -1,31 +1,41 @@
 package com.sparta.hotdeal.company.infrastructure.config;
 
+import com.sparta.hotdeal.company.infrastructure.exception.CustomAccessDeniedHandler;
+import com.sparta.hotdeal.company.infrastructure.exception.CustomAuthenticationHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.sparta.hotdeal.company.infrastructure.filter.AuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+@EnableWebSecurity //spring security 설정 활성화
+@EnableMethodSecurity(securedEnabled = true) //Secured 사용을 위해
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .sessionManagement((sessionManagement) ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/swagger-ui/**",       // Swagger UI 관련 정적 리소스
+                                "/v3/api-docs/**",      // OpenAPI 문서
+                                "/swagger-ui.html",     // Swagger HTML 경로
+                                "/swagger-resources/**", // Swagger 설정 리소스
+                                "/webjars/**"           // Swagger 정적 리소스
+                        ).permitAll()
+                        .anyRequest().authenticated() // 모든 요청 인증 필요 헤더가 없으면 401 리턴하기 위해
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new CustomAuthenticationHandler()) // 인증 실패 시 처리
+                        .accessDeniedHandler(new CustomAccessDeniedHandler()) // 권한 부족(403) 시 핸들러
+                )
+                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class); // 커스텀 필터 추가
 
         return http.build();
     }
