f37f9b4043ffafad4b9cfb4a9b533f485410b03e
MCshin00 <sinminc7@gmail.com>
2025-01-08 12:56:52 +0900

    refactor: SecurityAuditorAware Component 등록 (#45)

--- user/src/main/java/com/sparta/hotdeal/user/infrastructure/security/SecurityAuditorAware.java
+++ user/src/main/java/com/sparta/hotdeal/user/infrastructure/security/SecurityAuditorAware.java
@@ -3,7 +3,9 @@ package com.sparta.hotdeal.user.infrastructure.security;
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityAuditorAware implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
