a1ad8b16c44cb1a2c7f84595f007a4fd3a104a55
sooooooongyi <sooooooongyi@gmail.com>
2025-01-08 15:05:44 +0900

    feat: dto 내 유효성 검사 추가

--- company/src/main/java/com/sparta/hotdeal/company/application/dtos/company/ReqPostCompanyDto.java
+++ company/src/main/java/com/sparta/hotdeal/company/application/dtos/company/ReqPostCompanyDto.java
@@ -1,6 +1,7 @@
 package com.sparta.hotdeal.company.application.dtos.company;
 
 import com.sparta.hotdeal.company.domain.entity.company.CompanyStatusEnum;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
@@ -11,14 +12,15 @@ import lombok.Getter;
 @Builder
 public class ReqPostCompanyDto {
     @NotNull
+    @Digits(integer = 10, fraction = 0, message = "10자리 숫자만 가능합니다.")
     private Long businessRegistrationNumber;
-    @NotNull
+    @NotNull(message = "업체 번호를 입력해주세요.")
     private Long companyPhoneNumber;
-    @NotNull
+    @NotNull(message = "관리자 ID를 입력해주세요.")
     private UUID managerId;
-    @NotNull
+    @NotNull(message = "브랜드이름을 입력해주세요.")
     private String brandName;
-    @NotNull
+    @NotNull(message = "업체 이메일을 입력해주세요.")
     private String companyEmail;
     @NotNull
     private CompanyStatusEnum status;
