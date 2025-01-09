47e018fb16d39976d3a393601ecc757c486583b7
yewonkim301 <yeowne301@gmail.com>
2025-01-09 14:50:51 +0900

    chore: swagger 적용을 위한 config 작성(#59)

---
+++ product/src/main/java/com/sparta/hotdeal/product/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.sparta.hotdeal.product.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${openapi.service.url}") String url) {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(url)))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")))
+                .info(new Info().title("Product Service API")
+                        .version("v0.0.1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
