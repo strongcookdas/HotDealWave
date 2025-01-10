f68a50425fc06ab1573007fc181d016d99163636
yewonkim301 <yeowne301@gmail.com>
2025-01-10 12:16:38 +0900

    refactor: CompanyClient 호출할 때 마스터 권한주도록 수정(#59)

--- product/src/main/java/com/sparta/hotdeal/product/infrastructure/client/CompanyClient.java
+++ product/src/main/java/com/sparta/hotdeal/product/infrastructure/client/CompanyClient.java
@@ -2,13 +2,14 @@ package com.sparta.hotdeal.product.infrastructure.client;
 
 import com.sparta.hotdeal.product.application.dtos.res.ResponseDto;
 import com.sparta.hotdeal.product.application.service.client.CompanyClientService;
+import com.sparta.hotdeal.product.infrastructure.config.CompanyClientConfig;
 import com.sparta.hotdeal.product.infrastructure.dtos.ResGetCompanyByIdDto;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "company-service")
+@FeignClient(name = "company-service", configuration = CompanyClientConfig.class)
 public interface CompanyClient extends CompanyClientService {
 
     @GetMapping("/api/v1/companies/{companyId}")
@@ -16,7 +17,7 @@ public interface CompanyClient extends CompanyClientService {
 
     @Override
     default ResGetCompanyByIdDto getCompany(UUID companyId) {
-//        return this.getCompanyById(companyId).getData();
-        return ResGetCompanyByIdDto.createDummy();
+//        return ResGetCompanyByIdDto.createDummy();
+        return this.getCompanyById(companyId).getData();
     }
 }
