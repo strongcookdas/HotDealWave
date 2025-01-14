b74c1d8d9f9a5326274c6785083ee5db35c3e3f9
MCshin00 <sinminc7@gmail.com>
2025-01-14 21:12:03 +0900

    feat: 토큰 파싱 및 검증 로직 추가 (#107)

--- user/src/main/java/com/sparta/hotdeal/user/application/util/JwtUtil.java
+++ user/src/main/java/com/sparta/hotdeal/user/application/util/JwtUtil.java
@@ -1,17 +1,26 @@
 package com.sparta.hotdeal.user.application.util;
 
+import com.sparta.hotdeal.user.application.exception.CustomJwtExcpetion;
+import com.sparta.hotdeal.user.application.exception.JwtExceptionMessage;
 import com.sparta.hotdeal.user.domain.entity.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j(topic = "jwt")
 @Component
 public class JwtUtil {
     // Token 식별자
@@ -56,5 +65,31 @@ public class JwtUtil {
                 .setIssuedAt(date) // 발급일
                 .signWith(key, SignatureAlgorithm.HS256) // 암호화 알고리즘
                 .compact();
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            log.error(JwtExceptionMessage.INVALID_JWT_SIGNATURE.getMessage());
+            throw new CustomJwtExcpetion(JwtExceptionMessage.INVALID_JWT_SIGNATURE.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error(JwtExceptionMessage.EXPIRED_JWT_TOKEN.getMessage());
+            throw new CustomJwtExcpetion(JwtExceptionMessage.EXPIRED_JWT_TOKEN.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error(JwtExceptionMessage.UNSUPPORTED_JWT_TOKEN.getMessage());
+            throw new CustomJwtExcpetion(JwtExceptionMessage.UNSUPPORTED_JWT_TOKEN.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error(JwtExceptionMessage.JWT_CLAIM_IS_EMPTY.getMessage());
+            throw new CustomJwtExcpetion(JwtExceptionMessage.JWT_CLAIM_IS_EMPTY.getMessage());
+        }
     }
 }
