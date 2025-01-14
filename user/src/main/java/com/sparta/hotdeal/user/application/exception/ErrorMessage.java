d1ea13357e71fef2d31e367ad16380099078ced1
MCshin00 <sinminc7@gmail.com>
2025-01-14 21:10:46 +0900

    feat: refresh Token 관련 예외처리 추가 (#107)

--- user/src/main/java/com/sparta/hotdeal/user/application/exception/ErrorMessage.java
+++ user/src/main/java/com/sparta/hotdeal/user/application/exception/ErrorMessage.java
@@ -17,7 +17,9 @@ public enum ErrorMessage {
     ADDRESS_NOT_FOUND("주소를 찾을 수 없습니다."),
     NOT_ALLOWED_CONTENT("접근 권한이 없습니다."),
     DELETED_ADDRESS("삭제된 주소입니다."),
-    NOT_EXISTS_DEFAULT_ADDRESS("기본 배송지가 설정되지 않았습니다.");
+    NOT_EXISTS_DEFAULT_ADDRESS("기본 배송지가 설정되지 않았습니다."),
+    EXPIRED_REFRESH_TOKEN("만료된 리프레시 토큰입니다."),
+    NOT_VALID_REFRESH_TOKEN("리프레시 토큰 형식이 잘못되었습니다.");
 
     private final String message;
 }
