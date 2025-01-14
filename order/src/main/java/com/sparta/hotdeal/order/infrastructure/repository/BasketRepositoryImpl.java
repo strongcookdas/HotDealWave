d87e95a7eab55142a3fd428f071c1c26d3ae7e2b
strongcookdas <goodjav141592@gmail.com>
2025-01-14 12:04:29 +0900

    refactor : 장바구니 구현체 repository 생성 (#103)

--- order/src/main/java/com/sparta/hotdeal/order/infrastructure/repository/BasketRepositoryImpl.java
+++ order/src/main/java/com/sparta/hotdeal/order/infrastructure/repository/BasketRepositoryImpl.java
@@ -2,9 +2,42 @@ package com.sparta.hotdeal.order.infrastructure.repository;
 
 import com.sparta.hotdeal.order.domain.entity.basket.Basket;
 import com.sparta.hotdeal.order.domain.repository.BasketRepository;
+import com.sparta.hotdeal.order.infrastructure.repository.jpa.BasketRepositoryJpa;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
-public interface BasketRepositoryImpl extends JpaRepository<Basket, UUID>, BasketRepository {
+@Repository
+@RequiredArgsConstructor
+public class BasketRepositoryImpl implements BasketRepository {
+    private final BasketRepositoryJpa basketRepositoryJpa;
+
+    @Override
+    public Basket save(Basket basket) {
+        return basketRepositoryJpa.save(basket);
+    }
+
+    @Override
+    public List<Basket> findByIdInAndUserId(List<UUID> ids, UUID userId) {
+        return basketRepositoryJpa.findAllByIdInAndUserIdAndDeletedAtIsNull(ids, userId);
+    }
+
+    @Override
+    public Optional<Basket> findByIdAndUserId(UUID basketId, UUID userId) {
+        return basketRepositoryJpa.findByIdAndUserIdAndDeletedAtIsNull(basketId, userId);
+    }
+
+    @Override
+    public void delete(Basket basket) {
+
+    }
+
+    @Override
+    public Page<Basket> findAllByUserId(UUID userId, Pageable pageable) {
+        return basketRepositoryJpa.findAllByUserId(userId, pageable);
+    }
 }
---
+++ order/src/main/java/com/sparta/hotdeal/order/infrastructure/repository/jpa/BasketRepositoryJpa.java
@@ -0,0 +1,19 @@
+package com.sparta.hotdeal.order.infrastructure.repository.jpa;
+
+import com.sparta.hotdeal.order.domain.entity.basket.Basket;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface BasketRepositoryJpa extends JpaRepository<Basket, UUID> {
+    List<Basket> findAllByIdInAndUserIdAndDeletedAtIsNull(List<UUID> ids, UUID userId);
+
+    Optional<Basket> findByIdAndUserIdAndDeletedAtIsNull(UUID basketId, UUID userId);
+
+    Page<Basket> findAllByUserId(UUID userId, Pageable pageable);
+}
