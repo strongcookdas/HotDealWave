64d0371a8e278113b2f945a9049963ddaa9a744a
yewonkim301 <yeowne301@gmail.com>
2025-01-15 14:33:08 +0900

    refactor: Product 서비스 ConsumerApplicationKafkaConfig 설정(#104)

---
+++ product/src/main/java/com/sparta/hotdeal/product/infrastructure/config/ConsumerApplicationKafkaConfig.java
@@ -0,0 +1,42 @@
+package com.sparta.hotdeal.product.infrastructure.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class ConsumerApplicationKafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        // Kafka 브로커의 주소를 설정
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        // 메시지 키의 디시리얼라이저 클래스를 설정
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // 메시지 값의 디시리얼라이저 클래스를 설정
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // 설정된 프로퍼티로 DefaultKafkaConsumerFactory를 생성하여 반환
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        // 컨슈머 팩토리를 리스너 컨테이너 팩토리에 설정
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
