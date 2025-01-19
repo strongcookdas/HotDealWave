de9d3bee7713c19814476f6d0921b0fd15359fbb
pk_hyun <pkh954533@gmail.com>
2025-01-19 13:21:57 +0900

    fix: redissonConfig 하드코딩 된 호스트 부분 환경변수로 변경 (#122)

--- coupon/src/main/java/com/sparta/hotdeal/coupon/infrastructure/config/RedissonConfig.java
+++ coupon/src/main/java/com/sparta/hotdeal/coupon/infrastructure/config/RedissonConfig.java
@@ -2,17 +2,23 @@ package com.sparta.hotdeal.coupon.infrastructure.config;
 
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private String redisPort;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+                .setAddress("redis://" + redisHost + ":" + redisPort);
         return org.redisson.Redisson.create(config);
     }
 }
